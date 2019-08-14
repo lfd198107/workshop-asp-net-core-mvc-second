6b74d49544e96351f66ad1da06b7e7adcfc95607
Luiz Flavio <l.flaviod@yahoo.com.br>
2019-08-14 19:52:05 -0300

    CRUD Scaffolding

---
+++ SalesWebMvcSecond/Data/SalesWebMvcSecondContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesWebMvcSecond.Models
+{
+    public class SalesWebMvcSecondContext : DbContext
+    {
+        public SalesWebMvcSecondContext (DbContextOptions<SalesWebMvcSecondContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMvcSecond.Models.Department> Department { get; set; }
+    }
+}
--- SalesWebMvcSecond/Startup.cs
+++ SalesWebMvcSecond/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvcSecond.Models;
 
 namespace SalesWebMvcSecond
 {
@@ -33,6 +35,9 @@ namespace SalesWebMvcSecond
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<SalesWebMvcSecondContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcSecondContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
