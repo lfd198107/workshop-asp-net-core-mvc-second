dbba699cf02386180b0b7ca60e373f9aeb6b7932
Luiz Flavio <l.flaviod@yahoo.com.br>
2019-08-14 21:54:42 -0300

    MySQL adaptation and first migration

--- SalesWebMvcSecond/Startup.cs
+++ SalesWebMvcSecond/Startup.cs
@@ -37,7 +37,8 @@ namespace SalesWebMvcSecond
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddDbContext<SalesWebMvcSecondContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcSecondContext")));
+                    options.UseMySql(Configuration.GetConnectionString("SalesWebMvcSecondContext"), builder =>
+                    builder.MigrationsAssembly("SalesWebMvcSecond")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
